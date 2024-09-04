95612b6fb73ff51daa0ad2e8aabea53ea602ed15
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-04 14:34:40 +0530

    updated commit

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08a96114fe3e853c4ea32798bffbd304e8f8591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e626429d2a966a9ea38903f22d77b2acebb51bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
